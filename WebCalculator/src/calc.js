1467de4f21d54610f7f18319bd63901e32b252a8
Martin Schmidt <schmidtmartin697@gmail.com>
2021-11-13 23:46:13 +0100

    Fixed commit 951759f

--- WebCalculator/src/calc.js
+++ WebCalculator/src/calc.js
@@ -1,15 +1,27 @@
 const getDisplay = document.getElementById('display'); 
+var operators = ['+ ', '- ', '* ', '/ ']
+var special = ['.','%']
 
 function calculateResult() {
     getDisplay.innerHTML = eval(getDisplay.innerHTML);
 }
 
 function appendOperator(operator) {
-    document.getElementById('display').innerHTML += operator;
+    if(getDisplay.innerHTML == '') {
+        alert("Next input must be a number!");
+    }
+    else if(operators.includes(getDisplay.innerHTML.slice(getDisplay.innerHTML.length - 2))) {
+        alert('Next input must be a number!');
+    }
+    else if(special.includes(getDisplay.innerHTML.slice(getDisplay.innerHTML.length - 1))) {
+        alert("Next input must be a number!");
+    }else{
+        document.getElementById('display').innerHTML += operator;
+    }
 }
 
 function drawNumber(number) {
-    getDisplay.innerHTML += number;
+        getDisplay.innerHTML += number;
 }
 
 function DEL_Button() {
